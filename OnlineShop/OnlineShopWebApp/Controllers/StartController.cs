dc9965a4ff82d40d1ec6898a3b149b481b94a69e
Paul Pomogaev <paulslock1@gmail.com>
2025-09-08 11:23:36 +0400

    создал класс StartController ввёл переменную типа DateTime для выявления текущего времени

---
+++ OnlineShop/OnlineShopWebApp/Controllers/StartController.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class StartController : Controller
+    {
+        public string Hello()
+        {
+            var now = DateTime.Now;
+        }
+    }
+}
