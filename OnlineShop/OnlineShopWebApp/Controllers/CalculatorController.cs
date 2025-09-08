3b1fb88d4ad2fb9e5a5f37ad8eca91f4ba52eae9
Paul Pomogaev <paulslock1@gmail.com>
2025-09-08 13:32:26 +0400

    добавил проверку на дурака и условие через switch-выражение

--- OnlineShop/OnlineShopWebApp/Controllers/CalculatorController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/CalculatorController.cs
@@ -6,7 +6,23 @@ namespace OnlineShopWebApp.Controllers
     {
         public string Index(double? num1 = 0, double? num2 = 0, string operation = "+")
         {
-            return View();
+            double n1 = num1 ?? 0;
+            double n2 = num2 ?? 0;
+
+            if(operation != "+" && operation != "-" && operation != "*")
+            {
+                return "Ошибка, допустимо использование только трёх символов: + или - или *. Например, /calculator/index/1/3/+";
+            }
+
+            double result = operation switch
+            {
+                "+" => n1 + n2,
+                "-" => n1 - n2,
+                "*" => n1 * n2,
+                _ => n1 + n2
+            };
+
+            return $"{n1} {operation} {n2} = {result}";
         }
     }
 }
