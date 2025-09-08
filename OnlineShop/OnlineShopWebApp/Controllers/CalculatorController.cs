85c8a2ce3c78d49f7a906ee22ae4e660824a6ce0
Paul Pomogaev <paulslock1@gmail.com>
2025-09-08 14:53:02 +0400

    В методе Index контроллера CalculatorController  добавил проверку на использование соотвествующих знаков сложения, вычитания, умножения и деления ASP.NET Core делает декодировку автоматически. Сделал проверку деления на ноль, добавил условия через switch-выражение для переменной result

--- OnlineShop/OnlineShopWebApp/Controllers/CalculatorController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/CalculatorController.cs
@@ -6,7 +6,29 @@ namespace OnlineShopWebApp.Controllers
     {
         public string Index(double? a = 0, double? b = 0, string c = "+")
         {
-            
+            double num1 = a ?? 0;
+            double num2 = b ?? 0;
+            if(c != "+" && c != "-" && c != "*" && c != "/")
+            {
+                return "Ошибка, допускается использование только данных символов: + или - или * или /. Например, ?a=5&b=2&c=/";
+            }
+
+            if(c == "/" && num2 == 0)
+            {
+                return "Ошибка, делай что хочешь, но на ноль дельить нельзя";
+            }
+
+            double result = c switch
+            {
+                "+" => num1 + num2,
+                "-" => num1 - num2,
+                "*" => num1 * num2,
+                "/" => num1 / num2,
+                _ => num1 + num2
+            };
+
+            return $"{num1} {c} {num2} = {result}";
+
         }
     }
 }
