2bf6a24370012b2efce6e40d4870344ee188f3a8
Paul Pomogaev <paulslock1@gmail.com>
2025-09-08 13:55:35 +0400

    Добавил контроллер CalculatorController и метод public string Index с тремя параметрами

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(double? a = 0, double? b = 0, string c = "+")
+        {
+            
+        }
+    }
+}
