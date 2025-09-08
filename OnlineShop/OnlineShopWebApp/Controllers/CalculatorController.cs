e59c38216198eb0be6c8f7b92fb133e7667a9a2a
Paul Pomogaev <paulslock1@gmail.com>
2025-09-08 12:29:41 +0400

    добавил CalculatorController в раздел Controllers
    добавил параметры по-умолчанию double? num1 = 0, double? num2 = 0 и условие вывода

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CalculatorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(double? num1 = 0, double? num2 = 0)
+        {
+            double n1 = num1 ?? 0;
+            double n2 = num2 ?? 0;
+            var result = n1 + n2;
+
+            return $"{n1} + {n2} = {result}";
+        }
+    }
+}
