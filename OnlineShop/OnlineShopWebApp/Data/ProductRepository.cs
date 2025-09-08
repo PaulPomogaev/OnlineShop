e9ccb229936a2dcd88ac6ed55f6f218a137e94c5
Paul Pomogaev <paulslock1@gmail.com>
2025-09-08 16:47:52 +0400

    Добавил логику в класс ProductJsonRepository, он может хранить, сохранять и добавлять продукты в формате json

--- OnlineShop/OnlineShopWebApp/Data/ProductRepository.cs
+++ OnlineShop/OnlineShopWebApp/Data/ProductRepository.cs
@@ -1,6 +1,34 @@
-﻿namespace OnlineShopWebApp.Data
+﻿using OnlineShopWebApp.Models;
+using System.Text.Json;
+
+namespace OnlineShopWebApp.Data
 {
     public static class ProductJsonRepository
     {
+        private static string _filePath = "Data/products.json";
+
+        public static List<Product> GetAll()
+        {
+            if(!File.Exists(_filePath))
+            {
+                return new List<Product>();
+            }
+
+            var json = File.ReadAllText(_filePath);
+            return JsonSerializer.Deserialize<List<Product>>(json) ?? new List<Product>();
+        }
+
+        public static void SaveAll(List<Product> products)
+        {
+            var json = JsonSerializer.Serialize(products, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(_filePath, json);
+        }
+
+        public static void Add(Product product)
+        {
+            var products = GetAll();
+            products.Add(product);
+            SaveAll(products);
+        }
     }
 }
