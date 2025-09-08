59d523c70027a6c878eea9b2ef74934cb0f49c21
Paul Pomogaev <paulslock1@gmail.com>
2025-09-08 13:10:29 +0400

    Создал контроллер CalculatorController с тремя параметрами

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(double? num1 = 0, double? num2 = 0, string operation = "+")
+        {
+            return View();
+        }
+    }
+}
