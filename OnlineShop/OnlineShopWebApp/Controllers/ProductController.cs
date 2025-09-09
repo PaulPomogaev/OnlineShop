bd029612bd53c5644f94aaddaf00ef5c88177d73
Paul Pomogaev <paulslock1@gmail.com>
2025-09-09 16:26:13 +0400

    Добавил класс ProductController и реализовал метод Index. По-сути скопировал метод реализации из предыдущей задачи, только добавил поиск по id

---
+++ OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Data;
+using OnlineShopWebApp.Models;
+using System.Text;
+
+namespace OnlineShopWebApp.Controllers
+
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+            var product = ProductJsonRepository.GetAll().FirstOrDefault(p => p.Id == id);
+            if(product == null)
+            {
+                return Content($"Товар с ID {id} не найден");
+            }
+
+            var stringBuilder = new StringBuilder();
+            
+                stringBuilder.AppendLine($"Id{product.Id}");
+                stringBuilder.AppendLine($"Name{product.Name}");
+                stringBuilder.AppendLine($"Cost{product.Cost}");
+                stringBuilder.AppendLine();
+            
+            return Content(stringBuilder.ToString(), "text/plain");
+        }
+    }
+}
