eb5c5b91ac21925b9011318c95a40ec9cedf8aaa
Paul Pomogaev <paulslock1@gmail.com>
2025-09-11 15:18:42 +0400

    подправил модель Product по описанию урока

--- OnlineShop/OnlineShopWebApp/Models/Product.cs
+++ OnlineShop/OnlineShopWebApp/Models/Product.cs
@@ -2,10 +2,10 @@
 {
     public class Product
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public decimal Cost { get; set; }
-        public  string Description { get; set; }
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+        public required decimal Cost { get; set; }
+        public  string? Description { get; set; }
 
         public Product() { }
                 
