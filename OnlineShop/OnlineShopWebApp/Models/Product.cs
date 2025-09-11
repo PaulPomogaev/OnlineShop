d4eb403ac9acf2e569aee981f9eb5c9cd61995f2
Paul Pomogaev <paulslock1@gmail.com>
2025-09-11 15:51:22 +0400

    убрал конструктор

--- OnlineShop/OnlineShopWebApp/Models/Product.cs
+++ OnlineShop/OnlineShopWebApp/Models/Product.cs
@@ -6,8 +6,6 @@
         public required string Name { get; set; }
         public required decimal Cost { get; set; }
         public  string? Description { get; set; }
-
-        public Product() { }
-                
+               
     }
 }
