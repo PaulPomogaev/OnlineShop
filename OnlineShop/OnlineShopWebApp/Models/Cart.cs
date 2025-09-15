1a0b51f9da44a21301858d57fab086a04b8bee2b
Paul Pomogaev <paulslock1@gmail.com>
2025-09-15 14:47:18 +0400

    Добавил модели CartItem - одна позиция в корзине (товар, количество, общая стоимость), общая стоимость вычисляемая, что удобно при акциях или изменении цены.
    Добавил модель корзины Cart, содержащую коллекцию CartItem и общую стоимость

---
+++ OnlineShop/OnlineShopWebApp/Models/Cart.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineShopWebApp.Models
+{
+    public class Cart
+    {
+        public List<CartItem> Items { get; set; } = new();
+        public decimal TotalCost => Items.Sum(item => item.Cost);
+    }
+}
---
+++ OnlineShop/OnlineShopWebApp/Models/CartItem.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineShopWebApp.Models
+{
+    public class CartItem
+    {
+        public required Product Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal Cost => Product.Cost * Quantity; // всегда будет актуальной
+    }
+}
