5e96de33bb96e469f1b82a74b3326e4b1eaf294b
Paul Pomogaev <paulslock1@gmail.com>
2025-09-29 16:59:02 +0400

    Добавил в Header Панель аминистратора

---
+++ OnlineShop/OnlineShopWebApp/Controllers/AdminController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Панель администратора";
+            return View();
+        }
+
+        public IActionResult Orders()
+        {
+            ViewData["Title"] = "Заказы";
+            return View("Orders");
+        }
+
+        public IActionResult Users()
+        {
+            ViewData["Title"] = "Пользователи";
+            return View("Users");
+        }
+
+        public IActionResult Roles()
+        {
+            ViewData["Title"] = "Роли";
+            return View("Roles");
+        }
+
+        public IActionResult Products()
+        {
+            ViewData["Title"] = "Товары";
+            return View("Products");
+        }
+    }
+}
---
+++ OnlineShop/OnlineShopWebApp/Views/Shared/Components/AdminMenu/AdminMenuViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Views.Shared.Components.AdminMenu
+{
+    public class AdminMenuViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("AdminMenu");
+        }
+    }
+}
