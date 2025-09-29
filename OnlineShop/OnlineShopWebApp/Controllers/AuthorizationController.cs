c5d2be86692482d9ed12bc1dfb52632b19c1f5c5
Paul Pomogaev <paulslock1@gmail.com>
2025-09-29 14:14:56 +0400

    Сделал форму авторизации. Логика обработки данных пока отсуствует

---
+++ OnlineShop/OnlineShopWebApp/Controllers/AuthorizationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Register()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
