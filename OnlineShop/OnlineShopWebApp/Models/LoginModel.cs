4008718ea5860ba64921d39e6f3411af4c27e236
Paul Pomogaev <paulslock1@gmail.com>
2025-09-30 17:12:20 +0400

    убрал ?

--- OnlineShop/OnlineShopWebApp/Models/LoginModel.cs
+++ OnlineShop/OnlineShopWebApp/Models/LoginModel.cs
@@ -2,8 +2,8 @@
 {
     public class LoginModel
     {
-        public string? Login { get; set; }
-        public string? Password { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
         public bool RememberMe { get; set; }
     }
 }
