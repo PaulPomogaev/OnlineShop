62d5879017dba1fa5157aab7adf0e7e36512d87c
Paul Pomogaev <paulslock1@gmail.com>
2025-10-06 13:50:49 +0400

    Убрал вложенность в RegistrationController

--- OnlineShop/OnlineShopWebApp/Controllers/RegistrationController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/RegistrationController.cs
@@ -13,16 +13,18 @@ namespace OnlineShopWebApp.Controllers
         [HttpPost]
         public IActionResult Register(RegisterModel model)
         {
-            if(ModelState.IsValid)
+            if(!ModelState.IsValid)
             {
-                if(model.Password == model.Login)
-                {
-                    ModelState.AddModelError("Password", "Пароль не должен совпадать с логином.");
-                    return View("Index", model);
-                }
-                return RedirectToAction("Index", "Authorization");
+                return View("Index", model);
             }
-            return View("Index", model);
+
+            if (model.Password == model.Login)
+            {
+                ModelState.AddModelError("Password", "Пароль не должен совпадать с логином.");
+                return View("Index", model);
+            }
+            
+            return RedirectToAction("Index", "Authorization");
         }
     }
 }
