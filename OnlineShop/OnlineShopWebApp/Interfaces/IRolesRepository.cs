505d66be97be4f77a6e4bc844240feeff90be2de
Paul Pomogaev <paulslock1@gmail.com>
2025-10-08 16:53:54 +0400

    Реализовал модель Role Реализовал проверку свойства Name в модели Role согласно ТЗ Реализовал интерфейс IRolesRepository Реализовал под него сервис, работающий с json. Зарегестрировал сервис в Program.cs Добавил необходимые методы в контроллер Admin и реализовал эти методы Реализовал Views-Admin-Roles и Views-Admin-AddRole

---
+++ OnlineShop/OnlineShopWebApp/Interfaces/IRolesRepository.cs
@@ -0,0 +1,13 @@
+﻿using OnlineShopWebApp.Models;
+
+namespace OnlineShopWebApp.Interfaces
+{
+    public interface IRolesRepository
+    {
+        List<Role> GetAll();
+        void Add(Role role);
+        Role? GetById(int id);
+        void Delete(int id);
+        bool Exist(string roleName);
+    }
+}
---
+++ OnlineShop/OnlineShopWebApp/Models/Role.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopWebApp.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Необходимо ввести наименование роли")]
+        [Display(Name = "Наименование роли", Prompt = "Введите наименование роли")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Название роли должно быть от 2 до 50 символов")]
+        public string Name { get; set; }
+    }
+}
