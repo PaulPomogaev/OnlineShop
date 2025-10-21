08d16a58af6ca4d060f1b7b392c0065985fc1c40
Paul Pomogaev <paulslock1@gmail.com>
2025-10-21 14:18:41 +0400

    Переделал логику с NextId, пока логика вычисления работала с каждым сервисом отдельно, всё было нормально. Но когда эту логику использует несколько сервисов, то могут появлятся ошибки с правильным присвоением Id. Сделал, чтобы при добавлении Id вычислялся в методе Add и присваивался конкретной сущности.

---
+++ OnlineShop/OnlineShopWebApp/Data/BaseJsonRepository.cs
@@ -0,0 +1,65 @@
+﻿using OnlineShopWebApp.Interfaces;
+using OnlineShopWebApp.Models;
+using System.Text.Json;
+using System.Text;
+
+namespace OnlineShopWebApp.Data
+{
+    public abstract class BaseJsonRepository<T> : IRepository<T> where T : class, IBaseId
+    {
+        protected readonly string FilePath;
+        
+        protected BaseJsonRepository(string filePath)
+        {
+            FilePath = filePath;
+        }
+
+        protected List<T> GetAllInternal()
+        {
+            if (!File.Exists(FilePath))
+            {
+                return new List<T>();
+            }
+
+            var json = File.ReadAllText(FilePath, Encoding.UTF8);
+            return JsonSerializer.Deserialize<List<T>>(json) ?? new List<T>();
+        }
+        public List<T> GetAll()
+        {
+            return GetAllInternal();
+        }
+
+        private void SaveAll(List<T> items)
+        {
+            var json = JsonSerializer.Serialize(items, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(FilePath, json, Encoding.UTF8);
+        }
+
+        public void Add(T item)
+        {
+            var items = GetAllInternal();
+            var nextId = items.Any() ? items.Max(i => i.Id) + 1 : 1;
+            item.Id = nextId;
+            items.Add(item);
+            SaveAll(items);
+        }
+
+        public void Delete(int id)
+        {
+            var items = GetAllInternal();
+            var item = items.FirstOrDefault(p => p.Id == id);
+
+            if (item != null)
+            {
+                items.Remove(item);
+                SaveAll(items);
+            }
+        }
+
+        public T? GetById(int id)
+        {
+            var items = GetAllInternal();
+            return items.FirstOrDefault(i => i.Id == id);
+        }
+    }
+}
