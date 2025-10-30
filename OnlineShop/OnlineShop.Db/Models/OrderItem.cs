34cd6bcf40b3431a9591cc302038276e5185e585
Paul Pomogaev <paulslock1@gmail.com>
2025-10-30 16:38:16 +0400

    Упорядочил OrderItem

--- OnlineShop/OnlineShop.Db/Models/OrderItem.cs
+++ OnlineShop/OnlineShop.Db/Models/OrderItem.cs
@@ -5,12 +5,13 @@ namespace OnlineShop.Db.Models
     public class OrderItem
     {
         public int Id { get; set; }
-        public Product Product { get; set; } 
+
         public int Quantity { get; set; }
 
         public int OrderId { get; set; }
         public int ProductId { get; set; }
-        public Order? Order { get; set; }
 
+        public Product Product { get; set; }
+        public Order? Order { get; set; }
     }
 }
