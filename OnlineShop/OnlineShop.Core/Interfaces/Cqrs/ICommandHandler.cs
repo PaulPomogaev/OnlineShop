176af06c023305a2816342444b72973e60bf2f74
Paul Pomogaev <paulslock1@gmail.com>
2025-12-22 14:53:36 +0400

    удалил лишние using

--- OnlineShop/OnlineShop.Core/Interfaces/Cqrs/ICommandHandler.cs
+++ OnlineShop/OnlineShop.Core/Interfaces/Cqrs/ICommandHandler.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OnlineShop.Core.Interfaces.Cqrs
+﻿namespace OnlineShop.Core.Interfaces.Cqrs
 {
     public interface ICommandHandler<in TCommand, TResult> where TCommand : ICommand<TResult>
     {
