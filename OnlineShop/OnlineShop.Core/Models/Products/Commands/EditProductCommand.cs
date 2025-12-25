9c8de3dd359f429e44b40255a15442d2cfde12b6
Paul Pomogaev <paulslock1@gmail.com>
2025-12-25 16:40:11 +0400

    удалил лишние using

--- OnlineShop/OnlineShop.Core/Models/Products/Commands/EditProductCommand.cs
+++ OnlineShop/OnlineShop.Core/Models/Products/Commands/EditProductCommand.cs
@@ -1,9 +1,4 @@
 ﻿using OnlineShop.Core.Interfaces.Cqrs;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OnlineShop.Core.Models.Products.Commands
 {
